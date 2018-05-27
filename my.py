953cd0a7996f3a376956c8182c56009e1e2f7ea6
Gai Yu <yg1246@nyu.edu>
2018-05-27 23:15:03 +0800

    commit

--- my.py
+++ my.py
@@ -3,6 +3,14 @@ import numpy as np
 import torch as th
 
 
+def sparse_sp2th(matrix):
+    coo = matrix.tocoo()
+    rows = th.from_numpy(coo.row).long().view(1, -1)
+    cols = th.from_numpy(coo.col).long().view(1, -1)
+    data = th.from_numpy(coo.data).float()
+    return th.sparse.FloatTensor(th.cat((rows, cols), 0), data, coo.shape)
+
+
 def onehot(x, d):
     """
     Parameters
